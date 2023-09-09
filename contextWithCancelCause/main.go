2c8a83e0805ae334c4220582b6bc95c5d36a895d
Arun K <arthurbishop120@gmail.com>
2023-09-09 17:40:49 +0530

    some changes

--- contextWithCancelCause/main.go
+++ contextWithCancelCause/main.go
@@ -12,17 +12,6 @@ func operation1(ctx context.Context) error {
 	return errors.New("failed")
 }
 
-func operation2(ctx context.Context) {
-	select {
-	case <-time.After(500 * time.Millisecond):
-		fmt.Println("done")
-	case <-ctx.Done():
-		// We can get the error from the context
-		err := context.Cause(ctx)
-		fmt.Println("halted operation2 due to error: ", err)
-	}
-}
-
 func main() {
 	ctx := context.Background()
 	ctx, cancel := context.WithCancelCause(ctx)
@@ -36,5 +25,13 @@ func main() {
 	}()
 
 	// Run operation2 with the same context we use for operation1
-	operation2(ctx)
+
+	select {
+	case <-time.After(500 * time.Millisecond):
+		fmt.Println("done")
+	case <-ctx.Done():
+		// We can get the error from the context
+		err := context.Cause(ctx)
+		fmt.Println("halted operation2 due to error: ", err)
+	}
 }
